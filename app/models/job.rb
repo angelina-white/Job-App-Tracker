1fb2e3e926f795166fa27af459a089847acb0b9c
Angelina <angelinarwhite@gmail.com>
2022-07-14 12:06:03 -0400

    add validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,5 @@ class Job < ApplicationRecord
   belongs_to :offer
   belongs_to :user
   has_many :interviews, dependent: :destroy
+
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,3 +1,8 @@
 class Offer < ApplicationRecord
     has_many :jobs
+
+    validates :salary, numericality: true
+    validates :pto, numericality: true
+    validates :sickLeave, numericality: true
+    validates :bonus, numericality: true
 end
